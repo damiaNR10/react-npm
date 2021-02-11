99c28fcc97f23a20db525f2e2324d19c398beded
damiaNR10 <damianrog96@gmail.com>
2021-02-11 01:34:56 +0100

    Use scss for styling

--- timeboxing/src/index.js
+++ timeboxing/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from "./components/App.js";
-import "./styles/main.css";
+import "./styles/main.scss";
 
 ReactDOM.render(<App />, document.querySelector('#root'));
