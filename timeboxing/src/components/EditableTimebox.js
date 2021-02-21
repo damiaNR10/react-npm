86d8209d35233e09850c1f861a8d83b5afcc72df
damiaNR10 <damianrog96@gmail.com>
2021-02-21 20:29:14 +0100

    Extract EditableTimebox component

---
+++ timeboxing/src/components/EditableTimebox.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import TimeboxEditor from "./TimeboxEditor"
+import CurrentTimebox from "./CurrentTimebox";
+
+class EditableTimebox extends React.Component {
+    state = {
+        title: "Uczę się wyciągać stan w górę!",
+        totalTimeInMinutes: 20,
+        isEditable: true 
+    }
+    handleTitleChange = (event) => {
+        this.setState({
+            title: event.target.value,
+        });
+    };
+    handleTotalTimeInMinutesChange = (event) => {
+        this.setState({
+            totalTimeInMinutes: event.target.value,
+        });
+    };
+    handleConfirm = (event) => {
+        this.setState({
+            isEditable: false
+        });
+    }
+    handleEdit = (event) => {
+        this.setState({
+            isEditable: true
+        });
+    }
+    render() {
+        const {title, totalTimeInMinutes, isEditable} = this.state;
+        return (
+            <>
+                <TimeboxEditor 
+                    isEditable = {isEditable}
+                    onConfirm = {this.handleConfirm} 
+                    title = {title} totalTimeInMinutes = {totalTimeInMinutes}
+                    onTitleChange = {this.handleTitleChange}
+                    onTotalTimeInMinutesChange = {this.handleTotalTimeInMinutesChange}
+                />
+                <CurrentTimebox 
+                isEditable = {isEditable}
+                title = {title} 
+                totalTimeInMinutes = {totalTimeInMinutes}
+                onEdit = {this.handleEdit}/> 
+            </>
+        );
+    }
+}
+
+export default EditableTimebox;
