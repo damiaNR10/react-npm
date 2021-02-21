bb76e014e3f7449eca01e615b6c1165ca4f745d1
damiaNR10 <damianrog96@gmail.com>
2021-02-21 20:34:03 +0100

    Extract TimeboxCreator component

--- timeboxing/src/components/remaining-components.js
+++ timeboxing/src/components/remaining-components.js
@@ -1,65 +1,8 @@
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
 import classNames from 'classnames';
 
 import EditableTimebox from './EditableTimebox';
-
-class TimeboxCreator extends React.Component {
-
-    //Obsługa formularzy 1:
-    // state = {
-    //     title: "",
-    //     totalTimeInMinutes: ""
-    // }
-
-    // handleTitleChange = () => {
-    //     this.setState({title: event.target.value});
-    // }
-
-    // handleTotalTimeInMinutesChange = () => {
-    //     this.setState({totalTimeInMinutes: event.target.value});
-    // }
-
-    //Obsługa formularzy 2:
-    constructor(props) {
-        super(props);
-        this.titleInput = React.createRef();
-        this.totalTimeInMinutesInput = React.createRef();
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        this.props.onCreate({
-            id: uuidv4(),
-            title: this.titleInput.current.value, 
-            totalTimeInMinutes: this.totalTimeInMinutesInput.current.value
-        });
-        this.titleInput.current.value = "";
-        this.totalTimeInMinutesInput.current.value = "";
-    }
-
-    //Obsługa formularzy 1 cd:
-    // render() {
-    //     return (
-    //         <form onSubmit = {this.handleSubmit} className="TimeboxCreator">
-    //             <label htmlFor="">Co robisz?<input onChange = {this.handleTitleChange} type="text" value = {this.state.title} /></label><br />
-    //             <label htmlFor="">Ile minut?<input onChange = {this.handleTotalTimeInMinutesChange} type="number" value = {this.state.totalTimeInMinutes} /></label><br />
-    //             <button>Dodaj Timebox</button>
-    //         </form>
-    //     );
-    // }
-
-    //Obłusga formularzy 2 cd:
-    render() {
-        return (
-            <form onSubmit = {this.handleSubmit} className="TimeboxCreator">
-                <label htmlFor="">Co robisz?<input type="text" ref = {this.titleInput} /></label><br />
-                <label htmlFor="">Ile minut?<input type="number" ref = {this.totalTimeInMinutesInput} /></label><br />
-                <button>Dodaj Timebox</button>
-            </form>
-        );
-    }
-}
+import TimeboxCreator from './TimeboxCreator';
 
 class TimeboxList extends React.Component {
 
