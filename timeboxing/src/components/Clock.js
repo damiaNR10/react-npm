70273f1f35355682f241e4ffbfd516fd523a5ab7
damiaNR10 <damianrog96@gmail.com>
2021-03-05 00:26:45 +0100

    Defaulr props for Clock component

--- timeboxing/src/components/Clock.js
+++ timeboxing/src/components/Clock.js
@@ -4,4 +4,12 @@ function Clock({className ="", hours = 1, minutes = 2, seconds = 3, miliseconds 
     return <h2 className={"Clock " + className}>Pozostało {minutes > 9 ? minutes : "0" + minutes}:{seconds > 9 ? seconds : "0" + seconds}</h2>;
 }
 
+Clock.defaultProps = {
+    className: "",
+    hours: 1,
+    minutes: 2,
+    seconds: 3,
+    miliseconds: 4
+};
+
 export default Clock;
