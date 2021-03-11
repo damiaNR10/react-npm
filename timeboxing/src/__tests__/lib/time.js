81cf1b50b347982a03e005b1e2e1a96c3408cc02
damiaNR10 <damianrog96@gmail.com>
2021-03-12 00:20:19 +0100

    Grouped tests

--- timeboxing/src/__tests__/lib/time.js
+++ timeboxing/src/__tests__/lib/time.js
@@ -1,19 +1,28 @@
 import { getMinutesAndSecondsFromDurationInSeconds} from '../../lib/time.js'
 
-test("getMinutesAndSecondsFromDurationInSeconds works for 30 seconds", () => {
-    expect(
-        getMinutesAndSecondsFromDurationInSeconds(30)
-    ).toEqual([0, 30]);
-});
+describe("getMinutesAndSecondsFromDurationInSeconds", () => {
 
-test("getMinutesAndSecondsFromDurationInSeconds returns 30 seconds for 30 seconds duration", () => {
-    expect(
-        getMinutesAndSecondsFromDurationInSeconds(30)[1]
-    ).toBe(30);
-});
+    test("works for 30 seconds", () => {
+        expect(
+            getMinutesAndSecondsFromDurationInSeconds(30)
+        ).toEqual([0, 30]);
+    });
 
-test("getMinutesAndSecondsFromDurationInSeconds returns 0 minutes for 30 seconds duration", () => {
-    expect(
-        getMinutesAndSecondsFromDurationInSeconds(30)[0]
-    ).toBe(0);
+    test("returns 30 seconds for 30 seconds duration", () => {
+        expect(
+            getMinutesAndSecondsFromDurationInSeconds(30)[1]
+        ).toBe(30);
+    });
+
+    test("returns 0 minutes for 30 seconds duration", () => {
+        expect(
+            getMinutesAndSecondsFromDurationInSeconds(30)[0]
+        ).toBe(0);
+    });
+
+    test("works for 140 seconds", () => {
+        expect(
+            getMinutesAndSecondsFromDurationInSeconds(140)
+        ).toEqual([2, 20]);
+    });
 });
