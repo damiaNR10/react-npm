2aa6c3f05c98eeb9e2a5e8ed6c2ec227c7738ade
damiaNR10 <damianrog96@gmail.com>
2021-03-12 00:28:29 +0100

    Regrouped tests

--- timeboxing/src/__tests__/lib/time.js
+++ timeboxing/src/__tests__/lib/time.js
@@ -2,27 +2,33 @@ import { getMinutesAndSecondsFromDurationInSeconds} from '../../lib/time.js'
 
 describe("getMinutesAndSecondsFromDurationInSeconds", () => {
 
-    test("works for 30 seconds", () => {
-        expect(
-            getMinutesAndSecondsFromDurationInSeconds(30)
-        ).toEqual([0, 30]);
+    describe("for durations shorter than one minute", () => {
+
+        test("returns 30 seconds for 30 seconds duration", () => {
+            expect(
+                getMinutesAndSecondsFromDurationInSeconds(30)[1]
+            ).toBe(30);
+        });
+    
+        test("returns 0 minutes for 30 seconds duration", () => {
+            expect(
+                getMinutesAndSecondsFromDurationInSeconds(30)[0]
+            ).toBe(0);
+        });
     });
 
-    test("returns 30 seconds for 30 seconds duration", () => {
-        expect(
-            getMinutesAndSecondsFromDurationInSeconds(30)[1]
-        ).toBe(30);
-    });
-
-    test("returns 0 minutes for 30 seconds duration", () => {
-        expect(
-            getMinutesAndSecondsFromDurationInSeconds(30)[0]
-        ).toBe(0);
-    });
-
-    test("works for 140 seconds", () => {
-        expect(
-            getMinutesAndSecondsFromDurationInSeconds(140)
-        ).toEqual([2, 20]);
+    describe("for durations logner or equal to one minute", () => {
+        
+        test("returns 2 minutes and 20 seconds for 140 seconds duration", () => {
+            expect(
+                getMinutesAndSecondsFromDurationInSeconds(140)
+            ).toEqual([2, 20]);
+        });
+    
+        it('returns 1 minute for 60 seconds duration', () => {
+            expect(
+                getMinutesAndSecondsFromDurationInSeconds(60)
+            ).toEqual([1, 0]);
+        });
     });
 });
