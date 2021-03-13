37a3290d039fa4af483d79112476a1c5073bd3b2
damiaNR10 <damianrog96@gmail.com>
2021-03-13 21:10:35 +0100

    Clock component's grouped tests

--- timeboxing/src/__tests__/components/Clock.js
+++ timeboxing/src/__tests__/components/Clock.js
@@ -2,31 +2,27 @@ import React from 'react';
 import Clock from '../../components/Clock';
 import ReactDOM from 'react-dom';
 
+let root = null;
 describe("<Clock />", () => {
 
     describe("when given minutes and seconds", () => {
 
-        it("renders h2 element properly", () => {
-            const root = document.createElement("div");
+        beforeEach(() =>{
+            root = document.createElement("div");
             ReactDOM.render(
                 <Clock minutes = {20} seconds = {40} />, root
             );
+        });
+
+        it("renders h2 element properly", () => {
             expect(root.childNodes[0].nodeName).toEqual("H2");
         });
     
         it("sets a Clock className", () => {
-            const root = document.createElement("div");
-            ReactDOM.render(
-                <Clock minutes = {20} seconds = {40} />, root
-            );
             expect(root.childNodes[0].className).toMatch(/Clock/);
         });
     
         it("renders time properly", () => {
-            const root = document.createElement("div");
-            ReactDOM.render(
-                <Clock minutes = {20} seconds = {40} />, root
-            );
             expect(root.childNodes[0].textContent).toMatch(/20:40/);
         });
     });
