d0aa10b5c40c91d625e20a2e30284b19ad5c2404
damiaNR10 <damianrog96@gmail.com>
2021-03-15 00:15:49 +0100

    Testing with ReactTestRenderer

--- timeboxing/src/__tests__/components/Clock.js
+++ timeboxing/src/__tests__/components/Clock.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import Clock from '../../components/Clock';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 
 let root = null;
+let clockRenderer = null;
 describe("<Clock />", () => {
 
-    describe("when given minutes and seconds", () => {
+    describe("when given minutes and seconds (DOM)", () => {
 
         beforeEach(() =>{
             root = document.createElement("div");
             ReactDOM.render(
                 <Clock minutes = {20} seconds = {40} />, root
             );
+        });
+
+        it("renders properly", () => {
+            expect(root.childNodes[0].nodeName).toEqual("H2");
+            expect(root.childNodes[0].className).toMatch(/Clock/);
+            expect(root.childNodes[0].textContent).toMatch(/20:40/);
         });
 
         it("renders h2 element properly", () => {
@@ -24,6 +32,33 @@ describe("<Clock />", () => {
     
         it("renders time properly", () => {
             expect(root.childNodes[0].textContent).toMatch(/20:40/);
+        });
+    });
+
+    describe("when given minutes and seconds (TestRenderer)", () => {
+
+        beforeEach(() =>{
+            clockRenderer = renderer.create(
+                <Clock minutes = {20} seconds = {40} />
+            )
+        });
+
+        it("renders properly", () => {
+            expect(clockRenderer.toJSON().type).toEqual('h2');
+            expect(clockRenderer.toJSON().props).toMatchObject({'className': expect.stringMatching(/Clock/)});
+            expect(clockRenderer.toJSON().children).toEqual(expect.arrayContaining(["20", "40"]));
+        });
+
+        it("renders h2 element properly", () => {
+            expect(clockRenderer.toJSON().type).toEqual('h2');
+        });
+    
+        it("sets a Clock className", () => {
+            expect(clockRenderer.toJSON().props).toMatchObject({'className': expect.stringMatching(/Clock/)});
+        });
+    
+        it("renders time properly", () => {
+            expect(clockRenderer.toJSON().children).toEqual(expect.arrayContaining(["20", "40"]));
         });
     });
 
