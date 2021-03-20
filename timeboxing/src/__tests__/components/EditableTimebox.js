1e7fa511e2a45d98eda21b99bfef605cc5016011
damiaNR10 <damianrog96@gmail.com>
2021-03-20 19:35:19 +0100

    EditableTimebox component testing with Testing Library React

--- timeboxing/src/__tests__/components/EditableTimebox.js
+++ timeboxing/src/__tests__/components/EditableTimebox.js
@@ -1,9 +1,26 @@
-import { render } from '@testing-library/react';
+import { render, cleanup, screen, userEvent, fireEvent } from '@testing-library/react';
 import EditableTimebox from '../../components/EditableTimebox';
 
 describe("<EditableTimebox />", () => {
-    it("fails for now", () => {
+
+    afterEach(() => {
+        cleanup();
+    })
+
+    it("shows 'Edit' button", () => {
         const {debug, getByText} = render(<EditableTimebox />);
-        debug();
+        expect(() => {
+            getByText("Edytuj");
+        }).not.toThrow();
+    })
+
+    it("allows editing the timebox", () => {
+        const {debug, getByText} = render(<EditableTimebox />);
+        fireEvent.click(screen.getByText("Edytuj"));
+        //userEvent.click(screen.getByRole('button', {name: 'Edytuj'}));
+        fireEvent.click(screen.getByText(/zmiany/))
+        expect(() => {
+            getByText("Edytuj");
+        }).not.toThrow();
     })
 });
